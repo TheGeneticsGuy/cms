066fdec083fe54c7921473cb795c0a348a410ada
TheGeneticsGuy <aaronjtopping@gmail.com>
2025-10-03 12:20:41 -0700

    Fully implemented Structure for CMS project

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Contacts } from './contacts/contacts';
+import { Header } from './header';
+import { ContactList } from './contacts/contact-list/contact-list';
+import { ContactDetail } from './contacts/contact-detail/contact-detail';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Contacts,
+    Header,
+    ContactList,
+    ContactDetail
   ],
   imports: [
     BrowserModule,
---
+++ src/app/header.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cms-header',
+  standalone: false,
+  templateUrl: './header.html',
+  styleUrl: './app.css'
+})
+export class Header {
+
+}
