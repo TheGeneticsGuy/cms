4331b1899ea559e11e4b49610e44a928bbcd6da6
TheGeneticsGuy <aaronjtopping@gmail.com>
2025-10-07 09:14:48 -0700

    Fully working Week 3 implementation

--- src/app/messages/message-edit/message-edit.ts
+++ src/app/messages/message-edit/message-edit.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-edit',
@@ -7,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrl: './message-edit.css'
 })
 export class MessageEdit {
+    @ViewChild('subject') subject: ElementRef;
+    @ViewChild('msgText') msgText: ElementRef;
+    @Output() addMessageEvent = new EventEmitter<Message>();
+    currentSender: string = 'Your Name'; // Placeholder
+
+    onSendMessage() {
+    const subjectValue = this.subject.nativeElement.value;
+    const msgTextValue = this.msgText.nativeElement.value;
+
+    const newMessage = new Message(
+      '1', // hardcoding.. should this reference an incrememnting number?
+      subjectValue,
+      msgTextValue,
+      this.currentSender
+    );
+
+    this.addMessageEvent.emit(newMessage);
+    // this.onClear(); // Clearing from after sending it because it's nicer
+  }
+
+  onClear() {
+    this.subject.nativeElement.value = '';
+    this.msgText.nativeElement.value = '';
+  }
 
 }
--- src/app/messages/message-list/message-list.ts
+++ src/app/messages/message-list/message-list.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-list',
@@ -7,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './message-list.css'
 })
 export class MessageList {
+  messages: Message[] = [
+    new Message('1', 'Test Subject 1', 'Hey - Test msg1', 'Joe Smith'),
+    new Message('2', 'Test Subject 2', 'Hey - Test msg2', 'AJ Top'),
+    new Message('3', 'Test Subject 3', 'Hey - Test msg3', 'John Doe')
+  ];
 
+  onAddMessage(message: Message) {
+    this.messages.push(message);
+  }
 }
