dde4771dab0c7367b9dbe61716403b157c4c8615
TheGeneticsGuy <aaronjtopping@gmail.com>
2025-10-18 21:24:59 -0700

    Document service working - no cross comp yet

--- src/app/documents/document-list/document-list.ts
+++ src/app/documents/document-list/document-list.ts
@@ -1,5 +1,6 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Document } from '../document.model';
+import { DocumentService } from '../document.service';
 
 @Component({
   selector: 'cms-document-list',
@@ -7,41 +8,17 @@ import { Document } from '../document.model';
   templateUrl: './document-list.html',
   styleUrl: './document-list.css'
 })
-export class DocumentList {
-  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+export class DocumentList implements OnInit {
+  documents: Document[];
 
-  documents: Document[] = [
-    new Document(
-      '1',
-      'WDD 430',
-      'Full Stack Development',
-      // '../../assets/images/WDD430.webp',
-      'https://www.byui.edu/catalog/#/courses/VJkxTr9Ab?q=wdd%20430&&limit=20&skip=0&bc=true&bcCurrent=&bcCurrent=Web%20Full-Stack%20Development&bcItemType=courses',
-      null),
-    new Document(
-      '2',
-      'CSE 430',
-      'Software Architecture',
-      // '../../assets/images/CSE430.webp',
-      'https://www.byui.edu/catalog/#/courses/rJy7aiD2Q?q=CSE%20430&&limit=20&skip=0&bc=true&bcCurrent=&bcCurrent=Architectural%20Design&bcItemType=courses',
-      null),
-    new Document('3',
-      'CSE 499',
-      'Senior Project Capstone',
-      // '../../assets/images/CSE499.webp',
-      'https://www.byui.edu/catalog/#/courses/Ek-Px2lho-?q=CSE%20499&&limit=20&skip=0&bc=true&bcCurrent=&bcCurrent=Senior%20Project&bcItemType=courses',
-      null),
-    new Document(
-      '4',
-      'CSE 310',
-      'Applied Programming',
-      // '../../assets/images/CSE310.webp',
-      'https://www.byui.edu/catalog/#/courses/H1Rj-iP3X?q=CSE310&&limit=20&skip=0&bc=true&bcCurrent=&bcCurrent=Applied%20Programming&bcItemType=courses',
-      null)
-    ]
+  constructor(private documentService: DocumentService) {}
 
-    // For emitting the document to the html file
-    onSelectedDocument(document: Document) {
-      this.selectedDocumentEvent.emit(document);
-    }
+  ngOnInit() {
+    this.documents = this.documentService.getDocuments();
+  }
+
+  // For emitting the document to the html file
+  onSelectedDocument(document: Document) {
+    // this.selectedDocumentEvent.emit(document);
+  }
 }
---
+++ src/app/documents/document.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { MOCKDOCUMENTS } from './MOCKDOCUMENTS';
+import { Document } from './document.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DocumentService {
+  documents: Document[] = [];
+
+  constructor() {
+    this.documents = MOCKDOCUMENTS;
+  }
+
+  getDocuments(): Document[] {
+    return this.documents.slice();
+  }
+
+  getDocument(id: string): Document | null {
+    const document = this.documents.find(doc => doc.id === id);
+    return document || null;
+  }
+}
