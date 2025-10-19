0988ab35191bb35d6c05f098ad5668209aa9804d
TheGeneticsGuy <aaronjtopping@gmail.com>
2025-10-18 19:09:04 -0700

    Contact Service created and WORKING!!

---
+++ src/app/contacts/contact.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Contact } from './contact.model';
+import {MOCKCONTACTS} from './MOCKCONTACTS';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactService {
+  contacts: Contact[] = [];
+
+  constructor() {
+    this.contacts = MOCKCONTACTS;
+  }
+
+  getContacts(): Contact[] {
+    return this.contacts.slice();
+  }
+
+  getContact(id: string): Contact {
+    const contact = this.contacts.find(cont => cont.id === id)
+    return contact || null;
+  }
+}
