d3a681d4ff1cbb44fc618a8408a876e13ee7cc5a
TheGeneticsGuy <aaronjtopping@gmail.com>
2025-11-01 21:55:08 -0700

    Removed eventemitter imports

--- src/app/documents/document.service.ts
+++ src/app/documents/document.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, EventEmitter } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { MOCKDOCUMENTS } from './MOCKDOCUMENTS';
 import { Document } from './document.model';
 import { Subject } from 'rxjs';
