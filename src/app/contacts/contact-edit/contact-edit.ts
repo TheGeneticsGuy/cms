2312a231e01561dbed558464a60a68de98a9ad12
TheGeneticsGuy <aaronjtopping@gmail.com>
2025-11-25 02:55:49 -0700

    Drag and drop working

--- src/app/contacts/contact-edit/contact-edit.ts
+++ src/app/contacts/contact-edit/contact-edit.ts
@@ -3,6 +3,7 @@ import { Contact } from '../contact.model';
 import { ContactService } from '../contact.service';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
+import { CdkDragDrop } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'cms-contact-edit',
@@ -69,5 +70,49 @@ export class ContactEdit implements OnInit {
     this.router.navigate(['/contacts']);
   }
 
+  // Drag and Drop Methods
+  isInvalidContact(newContact: Contact) {
+    if (!newContact) {
+      return true;
+    }
+    if (this.contact && newContact.id === this.contact.id) {
+      return true;
+    }
+    for (let i = 0; i < this.groupContacts.length; i++) {
+      if (newContact.id === this.groupContacts[i].id) {
+        return true;
+      }
+    }
+    return false;
+  }
 
+  onDrop(event: CdkDragDrop<Contact[]>) {
+    if (event.previousContainer !== event.container) {
+      const contactCopy = { ...event.item.data }; // Needed tp create copy of data
+      const invalidGroupContact = this.isInvalidContact(contactCopy);
+
+      if (invalidGroupContact) {
+        return;
+      }
+
+      this.groupContacts.push(contactCopy);
+    }
+  }
+
+  // Keeping this code here as it was part of lesson, but Drag & Drop supplants it
+  // addToGroup(event: any) {
+  //   const selectedContact: Contact = event.dragData;
+  //   const invalidGroupContact = this.isInvalidContact(selectedContact);
+  //   if (invalidGroupContact){
+  //      return;
+  //   }
+  //   this.groupContacts.push(selectedContact);
+  // }
+
+  onRemoveItem(index: number) {
+    if (index < 0 || index >= this.groupContacts.length) {
+       return;
+    }
+    this.groupContacts.splice(index, 1);
+ }
 }
