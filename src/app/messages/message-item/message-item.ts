831170da0faaca7755781618403649dd2eaac3e6
TheGeneticsGuy <aaronjtopping@gmail.com>
2025-11-28 22:46:20 -0700

    Added some edit/delete buttons

--- src/app/messages/message-item/message-item.ts
+++ src/app/messages/message-item/message-item.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Message } from '../message.model';
 import { Contact } from '../../contacts/contact.model';
 import { ContactService } from '../../contacts/contact.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'cms-message-item',
@@ -13,7 +14,7 @@ export class MessageItem implements OnInit {
   @Input() message: Message;
   messageSender: string;
 
-  constructor(private contactService: ContactService) {}
+  constructor(private contactService: ContactService, private messageService: MessageService) {}
 
   ngOnInit(): void {
     // Initial attempt
@@ -21,6 +22,26 @@ export class MessageItem implements OnInit {
        const contact: Contact = this.contactService.getContact(this.message.sender);
        this.messageSender = contact ? contact.name : 'Unknown Sender';
     });
+  }
 
+  onDelete() {
+    if (confirm('Are you sure you want to delete this message?')) {
+      this.messageService.deleteMessage(this.message);
+    }
+  }
+
+  onEdit() {
+    const newText = prompt('Edit Message Text:', this.message.msgText);
+
+    if (newText !== null && newText !== this.message.msgText) {
+      const newMessage = new Message(
+        this.message.id,
+        this.message.subject,
+        newText,
+        this.message.sender
+      );
+
+      this.messageService.updateMessage(this.message, newMessage);
+    }
   }
 }
